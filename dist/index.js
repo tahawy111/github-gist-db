84ac378641ca7a43f478e1c195f84f830f449002
tahawy111 <amertahawy111@gmail.com>
2024-02-27 22:38:26 +0200

    Add Create Method

--- dist/index.js
+++ dist/index.js
@@ -13,166 +13,85 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
-function createGist() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const res = yield axios_1.default.post("https://api.github.com/gists", {
-            description: "test gist",
-            public: false,
-            files: {
-                "sell_goods.categories.json": {
-                    content: `[{
-            "_id": {
-              "$oid": "62a4b27ab3a6b9868cbb5cbc"
-            },
-            "category": "الكل",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a51711b3a6b9868cbb5d55"
-            },
-            "category": "اكسسوارات",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a51725b3a6b9868cbb5d5f"
-            },
-            "category": "هاردوير",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a51743b3a6b9868cbb5d69"
-            },
-            "category": "ميموري & فلاشات",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a51761b3a6b9868cbb5d73"
-            },
-            "category": "اجهزة النت والشبكات",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a5178eb3a6b9868cbb5d7d"
-            },
-            "category": "لاب توب",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a517c1b3a6b9868cbb5d87"
-            },
-            "category": "لاب توب استعمال",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a517e9b3a6b9868cbb5d97"
-            },
-            "category": "كاميرات المراقبه",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a51814b3a6b9868cbb5da1"
-            },
-            "category": "الاسلاك",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a5182db3a6b9868cbb5dae"
-            },
-            "category": "اجهزة الدش",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a5187ab3a6b9868cbb5dbe"
-            },
-            "category": "اكسسوارات الكمبيوتر ",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a5188cb3a6b9868cbb5dd0"
-            },
-            "category": "اكسسوارات الموبايل",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a518f5b3a6b9868cbb5dda"
-            },
-            "category": "الخدمات والصيانه",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a5191cb3a6b9868cbb5dea"
-            },
-            "category": "الشاشات",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a51c31b3a6b9868cbb5e0b"
-            },
-            "category": "ريموت كنترول",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a51cd8b3a6b9868cbb5e2d"
-            },
-            "category": "الاجهزه والاكسسوارات الاورجينال",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62a523a2b3a6b9868cbb5e9c"
-            },
-            "category": "الصبات والاسبيكرات",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62ab5d9bb7771fa3d4e495a0"
-            },
-            "category": "الكهرباء",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62c4ad2f4db3cca2fe8f03f3"
-            },
-            "category": "هاردوير كمبيوتر",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "62d2bd68dce409cb3f5520bb"
-            },
-            "category": "كمبيوتر استيراد",
-            "__v": 0
-          },
-          {
-            "_id": {
-              "$oid": "63362743d4290815875a6d38"
-            },
-            "category": "اكسسوارات البلايستيشن",
-            "__v": 0
-          }]`,
-                },
-            },
-        }, { headers: {
-                Authorization: "Bearer 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"
-            } });
-        console.log(res.data);
-    });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+class DB {
+    // Constructor
+    constructor(schema, { schemaName, projectName, gistId, timeStamps, }) {
+        // Properties
+        this.url = "https://api.github.com/gists";
+        this.schema = schema;
+        this.schemaName = schemaName;
+        this.projectName = projectName;
+        this.gistId = gistId;
+        this.timeStamps = timeStamps;
+    }
+    create(payload) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let reqPayload = Object.assign(Object.assign({}, payload), { id: crypto.randomUUID() });
+            if (this.timeStamps) {
+                reqPayload.createdAt = new Date().toISOString();
+                reqPayload.updatedAt = new Date().toISOString();
+            }
+            if (this.gistId) {
+                // get and push
+                const res = yield axios_1.default.get(`${this.url}/${this.gistId}`, {
+                    headers: {
+                        Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+                    },
+                });
+                const list = JSON.parse(res.data.files["test.productSchema.json"].content);
+                list.push(reqPayload);
+                const update = yield axios_1.default.patch(`${this.url}/${this.gistId}`, {
+                    files: {
+                        [`${this.projectName}.${this.schemaName}.json`]: {
+                            content: `${JSON.stringify(list)}`,
+                        },
+                    },
+                }, {
+                    headers: {
+                        Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+                    },
+                });
+                return update.data;
+            }
+            else {
+                // create first object inside and array
+                const res = yield axios_1.default.post(`${this.url}`, {
+                    description: `
+          Project: ${this.projectName}
+          && Schema: ${this.schemaName}
+          `,
+                    public: false,
+                    files: {
+                        [`${this.projectName}.${this.schemaName}.json`]: {
+                            content: `[${JSON.stringify(reqPayload)}]`,
+                        },
+                    },
+                }, {
+                    headers: {
+                        Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+                    },
+                });
+                return res.data;
+            }
+        });
+    }
 }
-createGist();
+const productSchema = new DB({
+    name: "String",
+    price: "Number",
+}, {
+    schemaName: "productSchema",
+    projectName: "test",
+    gistId: "48ec463b54be5973729a108297860555",
+    timeStamps: true,
+});
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    const product = yield productSchema.create({
+        id: "uuid",
+        name: "Product Name",
+        price: 100,
+    });
+    console.log(product);
+}))();
--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,167 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 interface SchemaTypes {
-    [key: string]:
-      | "String"
-      | "Number"
-      | "Boolean"
-      | "Object"
-      | "Array"
-      | "Undefined"
-      | "Null"
-      | "Symbol"
-      | "BigInt";
-  }
-  
-  type SchemaType<T> = {
-    [K in keyof T]: T[K] extends "String"
-      ? string
-      : T[K] extends "Number"
-      ? number
-      : T[K] extends "Boolean"
-      ? boolean
-      : T[K] extends "Object"
-      ? Object
-      : T[K] extends "Array"
-      ? Array<any>
-      : T[K] extends "Undefined"
-      ? undefined
-      : T[K] extends "Null"
-      ? null
-      : T[K] extends "Symbol"
-      ? Symbol
-      : T[K] extends "BigInt"
-      ? bigint
-      : any; // Default to 'any' for other types
-  };
-  
-  class DB<T extends SchemaTypes> {
-    // Properties
-    schema: T;
-  
-    // Constructor
-    constructor(schema: T) {
-      this.schema = schema;
+  [key: string]:
+    | "String"
+    | "Number"
+    | "Boolean"
+    | "Object"
+    | "Array"
+    | "Undefined"
+    | "Null"
+    | "Symbol"
+    | "BigInt";
+}
+
+type SchemaType<T> = {
+  [K in keyof T]: T[K] extends "String"
+    ? string
+    : T[K] extends "Number"
+    ? number
+    : T[K] extends "Boolean"
+    ? boolean
+    : T[K] extends "Object"
+    ? Object
+    : T[K] extends "Array"
+    ? Array<any>
+    : T[K] extends "Undefined"
+    ? undefined
+    : T[K] extends "Null"
+    ? null
+    : T[K] extends "Symbol"
+    ? Symbol
+    : T[K] extends "BigInt"
+    ? bigint
+    : any; // Default to 'any' for other types
+} & { id: "uuid" };
+
+class DB<T extends SchemaTypes> {
+  // Properties
+  private url = "https://api.github.com/gists";
+  schema: T;
+  schemaName: string;
+  projectName: string;
+  gistId?: string;
+  timeStamps?: boolean;
+  githubToken: string;
+
+  // Constructor
+  constructor(
+    schema: T,
+    {
+      schemaName,
+      projectName,
+      gistId,
+      timeStamps,
+      githubToken,
+    }: {
+      schemaName: string;
+      projectName: string;
+      gistId?: string;
+      timeStamps?: boolean;
+      githubToken: string;
     }
-  
-    create(payload: SchemaType<T>) {
-      // Implementation of create method
+  ) {
+    this.schema = schema;
+    this.schemaName = schemaName;
+    this.projectName = projectName;
+    this.gistId = gistId;
+    this.timeStamps = timeStamps;
+    this.githubToken = githubToken;
+  }
+
+  async create(payload: SchemaType<T>) {
+    let reqPayload: any = {
+      ...payload,
+      id: crypto.randomUUID(),
+    };
+
+    if (this.timeStamps) {
+      reqPayload.createdAt = new Date().toISOString();
+      reqPayload.updatedAt = new Date().toISOString();
+    }
+
+    if (this.gistId) {
+      // get and push
+      const res = await axios.get(`${this.url}/${this.gistId}`, {
+        headers: {
+          Authorization: `Bearer ${this.githubToken}`,
+        },
+      });
+
+      const list: SchemaType<T>[] = JSON.parse(
+        res.data.files["test.productSchema.json"].content
+      );
+
+      list.push(reqPayload);
+
+      const update = await axios.patch(
+        `${this.url}/${this.gistId}`,
+        {
+          files: {
+            [`${this.projectName}.${this.schemaName}.json`]: {
+              content: `${JSON.stringify(list)}`,
+            },
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${this.githubToken}`,
+          },
+        }
+      );
+
+      return update.data;
+    } else {
+      // create first object inside and array
+      const res = await axios.post(
+        `${this.url}`,
+        {
+          description: `
+          Project: ${this.projectName}
+          && Schema: ${this.schemaName}
+          `,
+          public: false,
+          files: {
+            [`${this.projectName}.${this.schemaName}.json`]: {
+              content: `[${JSON.stringify(reqPayload)}]`,
+            },
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${this.githubToken}`,
+          },
+        }
+      );
+      return res.data;
     }
   }
-  
-  const productSchema = new DB({
+}
+
+const productSchema = new DB(
+  {
     name: "String",
     price: "Number",
+  },
+  {
+    githubToken: process.env.GITHUB_ACCESS_TOKEN!,
+    schemaName: "productSchema",
+    projectName: "test",
+    gistId: "48ec463b54be5973729a108297860555",
+    timeStamps: true,
+  }
+);
+
+(async () => {
+  const product = await productSchema.create({
+    id: "uuid",
+    name: "Product Name",
+    price: 100,
   });
-  
-  productSchema.create({
-      name: "Product Name",
-      price: 100
-  });
+
+  console.log(product);
+})();
